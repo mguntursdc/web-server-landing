68c6fb5fa99b839223518a6098ee0f6ff825ae5a
bitbucket-mguntursdc <user-guntur.muhammad@happyfresh.com>
2022-05-24 22:24:16 +0700

    modify shellscript

--- landing-server-setup.sh
+++ landing-server-setup.sh
@@ -2,6 +2,10 @@
 
 sudo apt-get update
 
+sudo su
+cd
+git clone https://github.com/sdcilsy/landing-page
+
 systemctl status nginx | grep -E "*running"
 
 if [ $? -eq 0 ]; then
@@ -10,10 +14,6 @@ else
         echo "NGINX belum terinstall"
         sudo apt-get install -y nginx
 fi
-
-sudo su
-cd
-git clone https://github.com/sdcilsy/landing-page
 
 rm /var/www/html/index.nginx-debian.html
 sudo mv landing-page/* /var/www/html/
